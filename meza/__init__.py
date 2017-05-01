1845acec27fd80f53746841d3323b910eaa0aadc
Reuben Cummings <reubano@gmail.com>
2017-05-01 18:41:25 +0300

    Bump to version 0.37.0

--- meza/__init__.py
+++ meza/__init__.py
@@ -21,7 +21,7 @@ from __future__ import (
 from datetime import datetime as dt
 from os import path as p
 
-__version__ = '0.36.2'
+__version__ = '0.37.0'
 __title__ = 'meza'
 __package_name__ = 'meza'
 __author__ = 'Reuben Cummings'

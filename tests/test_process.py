8d5bc35b14a3900fd77ea741ee4a8bd52400c4a7
Jason R. Coombs <jaraco@jaraco.com>
2024-02-19 03:14:50 -0500

    Ran nose2pytest

--- tests/test_process.py
+++ tests/test_process.py
@@ -31,7 +31,7 @@ class Test:
     def test_typecast(self):
         records = [{"float": "1.5"}]
         types = [{"id": "float", "type": "bool"}]
-        assert_equal({"float": False}, next(pr.type_cast(records, types)))
+        assert {"float": False} == next(pr.type_cast(records, types))
 
         with assert_raises(ValueError):
             next(pr.type_cast(records, types, warn=True))
@@ -50,9 +50,9 @@ class Test:
 
         records = it.repeat(record)
         records, result = pr.detect_types(records)
-        assert_equal(17, result["count"])
-        assert_equal(Decimal("0.95"), result["confidence"])
-        assert_true(result["accurate"])
+        assert 17 == result["count"]
+        assert Decimal("0.95") == result["confidence"]
+        assert result["accurate"]
 
         expected = {
             "null": "null",
@@ -65,23 +65,23 @@ class Test:
             "datetime": "datetime",
         }
 
-        assert_equal(expected, {r["id"]: r["type"] for r in result["types"]})
-        assert_equal(record, next(records))
+        assert expected == {r["id"]: r["type"] for r in result["types"]}
+        assert record == next(records)
 
         result = pr.detect_types(records, 0.99)[1]
-        assert_equal(100, result["count"])
-        assert_equal(Decimal("0.97"), result["confidence"])
-        assert_false(result["accurate"])
+        assert 100 == result["count"]
+        assert Decimal("0.97") == result["confidence"]
+        assert not result["accurate"]
 
         result = pr.detect_types([record, record])[1]
-        assert_equal(2, result["count"])
-        assert_equal(Decimal("0.87"), result["confidence"])
-        assert_false(result["accurate"])
+        assert 2 == result["count"]
+        assert Decimal("0.87") == result["confidence"]
+        assert not result["accurate"]
 
     def test_detect_types_datetimes_midnight(self):
         records = it.repeat({"foo": "2000-01-01 00:00:00"})
         records, result = pr.detect_types(records)
-        assert_equal(result["types"], [{"id": "foo", "type": "datetime"}])
+        assert result["types"] == [{"id": "foo", "type": "datetime"}]
 
     def test_fillempty(self):
         records = [
@@ -100,30 +100,30 @@ class Test:
         more_values_1 = [values[0], new_value_1, values[2]]
 
         fields = ["a"]
-        assert_equal(values, list(pr.fillempty(records, 0, fields=fields)))
+        assert values == list(pr.fillempty(records, 0, fields=fields))
 
         filled = pr.fillempty(records, method="front")
-        assert_equal(more_values_1, list(filled))
+        assert more_values_1 == list(filled)
 
         new_value_2 = {"a": "1", "b": "27", "c": "17"}
         new_value_3 = {"a": "0", "b": "too short!", "c": "17"}
         more_values_2 = [new_value_2, new_value_3, values[2]]
         filled = pr.fillempty(records, method="back")
-        assert_equal(more_values_2, list(filled))
+        assert more_values_2 == list(filled)
 
         more_values_3 = [values[0], new_value_3, values[2]]
         filled = pr.fillempty(records, method="back", limit=1)
-        assert_equal(more_values_3, list(filled))
+        assert more_values_3 == list(filled)
 
         kwargs = {"method": "b", "fields": ["a"]}
         new_value_4 = {"a": "too short!", "b": "too short!", "c": None}
         more_values_4 = [values[0], new_value_4, values[2]]
-        assert_equal(more_values_4, list(pr.fillempty(records, **kwargs)))
+        assert more_values_4 == list(pr.fillempty(records, **kwargs))
 
     def test_merge(self):
         expected = {"a": 1, "b": 10, "c": 11}
         result = pr.merge([{"a": 1, "b": 2}, {"b": 10, "c": 11}])
-        assert_equal(expected, result)
+        assert expected == result
 
         # setup
         records = [{"a": 1, "b": 2, "c": 3}, {"b": 4, "c": 5, "d": 6}]
@@ -131,29 +131,29 @@ class Test:
         # Combine all keys
         expected = {"a": 1, "c": 8, "b": 6, "d": 6}
         result = pr.merge(records, pred=bool, op=sum)
-        assert_equal(expected, result)
+        assert expected == result
 
         first = lambda pair: next(filter(partial(is_not, None), pair))
         kwargs = {"pred": bool, "op": first, "default": None}
         expected = {"a": 1, "b": 2, "c": 3, "d": 6}
         result = pr.merge(records, **kwargs)
-        assert_equal(expected, result)
+        assert expected == result
 
         # This will only reliably give the expected result for 2 records
         kwargs = {"pred": bool, "op": stats.mean, "default": None}
         expected = {"a": 1, "b": 3.0, "c": 4.0, "d": 6.0}
         result = pr.merge(records, **kwargs)
-        assert_equal(expected, result)
+        assert expected == result
 
         # Only combine key 'b'
         expected = {"a": 1, "b": 6, "c": 5, "d": 6}
         result = pr.merge(records, pred="b", op=sum)
-        assert_equal(expected, result)
+        assert expected == result
 
         # Only combine keys that have the same value of 'b'
         expected = {"a": 1, "b": 6, "c": 5, "d": 6}
         result = pr.merge(records, pred=itemgetter("b"), op=sum)
-        assert_equal(expected, result)
+        assert expected == result
 
         # This will reliably work for any number of records
         counted = defaultdict(int)
@@ -169,23 +169,23 @@ class Test:
                 counted[k] += 1
 
         expected = {"a": 3, "b": 3, "c": 2, "d": 1}
-        assert_equal(expected, counted)
+        assert expected == counted
 
         summed = pr.merge(records, pred=bool, op=sum)
         expected = {"a": 6, "b": 15, "c": 2, "d": 7}
-        assert_equal(expected, summed)
+        assert expected == summed
 
         kwargs = {"pred": bool, "op": ft.fpartial(truediv)}
         expected = {"a": 2.0, "b": 5.0, "c": 1.0, "d": 7.0}
         result = pr.merge([summed, counted], **kwargs)
-        assert_equal(expected, result)
+        assert expected == result
 
         # This should also reliably work for any number of records
         op = ft.fpartial(ft.sum_and_count)
         kwargs = {"pred": bool, "op": op, "default": None}
         merged = pr.merge(records, **kwargs)
         result = {x: truediv(*y) for x, y in merged.items()}
-        assert_equal(expected, result)
+        assert expected == result
 
     def test_unique(self):
         records = [
@@ -202,7 +202,7 @@ class Test:
 
         pred = lambda x: x["name"][0]
         result = next(it.islice(pr.unique(records, pred=pred), 3, 4))["name"]
-        assert_equal("rob", result)
+        assert "rob" == result
 
     def test_cut(self):
         records = [
@@ -213,10 +213,10 @@ class Test:
 
         expected = {"field_1": 1, "field_3": "male"}
         result = next(pr.cut(records, ["field_2"], exclude=True))
-        assert_equal(expected, result)
+        assert expected == result
 
         result = next(pr.cut(records, ["field_2"]))
-        assert_equal({"field_2": "bill"}, result)
+        assert {"field_2": "bill"} == result
 
     def test_grep(self):
         records = [
@@ -229,19 +229,19 @@ class Test:
 
         rules = [{"fields": ["day"], "pattern": partial(eq, 1)}]
         result = next(pr.grep(records, rules))["name"]
-        assert_equal("bill", result)
+        assert "bill" == result
 
         rules = [{"pattern": partial(contains, {1, "rob"})}]
         result = next(pr.grep(records, rules))["name"]
-        assert_equal("rob", result)
+        assert "rob" == result
 
         rules = [{"pattern": partial(contains, {1, "rob"})}]
         result = next(pr.grep(records, rules, any_match=True))["name"]
-        assert_equal("bill", result)
+        assert "bill" == result
 
         rules = [{"fields": ["name"], "pattern": "o"}]
         result = next(pr.grep(records, rules, inverse=True))["name"]
-        assert_equal("bill", result)
+        assert "bill" == result
 
     def test_pivot(self):
         records = [
@@ -266,4 +266,4 @@ class Test:
         result = list(pr.pivot(records, "D", "C", dropna=False))
         expected_set = set(tuple(sorted(r.items())) for r in expected)
         result_set = set(tuple(sorted(r.items())) for r in result)
-        assert_equal(expected_set, result_set)
+        assert expected_set == result_set

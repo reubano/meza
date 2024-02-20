275870f7cdfe29a0ae32db81717814e220bd61d7
Jason R. Coombs <jaraco@jaraco.com>
2024-02-19 20:04:15 -0500

    Bump version for next release

--- meza/__init__.py
+++ meza/__init__.py
@@ -19,7 +19,7 @@ Attributes:
 from datetime import datetime as dt
 from os import path as p
 
-__version__ = "0.46.0"
+__version__ = "0.47.0"
 __title__ = "meza"
 __package_name__ = "meza"
 __author__ = "Reuben Cummings"
